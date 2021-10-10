9cbd4989ffcbd3f233c5141563f2e618b8d410ae
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-10-10 18:10:59 +0530

    navbar in mobile view

--- Script/script.js
+++ Script/script.js
@@ -3,3 +3,25 @@ var preloader = document.getElementById('loader');
 function loaded(){
     preloader.style.display = 'none';
 }
+
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  menu.classList.add("active");
+  menuBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+  body.classList.add("disabledScroll");
+}
+cancelBtn.onclick = ()=>{
+  menu.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  cancelBtn.classList.remove("show");
+  body.classList.remove("disabledScroll");
+}
+
+window.onscroll = ()=>{
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
